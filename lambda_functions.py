e24183957b265875fa91820696231c4e45f16f26
abhi <programwithabhi@gmail.com>
2023-01-20 20:35:07 +0530

    learned the lambda functions

---
+++ lambda_functions.py
@@ -0,0 +1,4 @@
+# lambda functions in the python are the convinient way to write short hand functions 
+abhi = lambda x: x+1
+
+print(abhi(2))
---
+++ map.py
@@ -0,0 +1,2 @@
+# map is a higher order function in python that take two arguments first is a function and the second is iterable object like a list or tuple
+
